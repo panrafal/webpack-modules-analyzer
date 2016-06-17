9e1f290b0c727701d4ab8bbaf3cad97f794df8d0
Rafal Lindemann <rl@stamina.pl>
2016-06-17 12:03:18 +0200

    Things

--- cli.js
+++ cli.js
@@ -8,6 +8,28 @@ var toJSON = require('./lib/toJSON')
 var toCSV = require('./lib/toCSV')
 
 var statsFile = args._[0]
+
+if (!statsFile || args.help || !(args.json || args.csv || args.inspect)) {
+  help = [
+    'Webpack Stats file analyzer',
+    '\nUsage:',
+    '  webpack-modules-analyzer  [other options] build-stats.json',
+    '\nOutput options: ',
+    '  --inspect - print out results to console',
+    '  --json=FILE - print out results to json file',
+    '  --csv=FILE - print out results to csv file',
+    '  --verbose - include all dependent modules in output',
+    '  --sortBy=KEY - sort by specified key (defaults to allSize)',
+    '\nAnalysis: ',
+    '  --gzip - count gzipped module size',
+    '  --limit=X - limit results to top X item',
+    '  --match=REGEXP - limit results to modules with name matching REGEXP',
+    '  --chunk=CHUNK - limit results to modules in CHUNK',
+  ]
+  console.log(help.join('\n'))
+  return
+}
+
 var stats = JSON.parse(fs.readFileSync(statsFile))
 var options = args
 
@@ -22,6 +44,7 @@ if (args.json) {
 if (args.csv) {
   toCSV(json, fs.createWriteStream(args.csv))
 }
+
 if (args.inspect) {
   console.log(util.inspect(json, {colors: true, depth: 4}))
 }
